449ea8e87b1e3ec1209f1a38fc36a5bf04a71a04
FreeZBee1212 <warcraft.creater@mail.ru>
2024-05-06 18:45:48 +0300

    Comm last change front

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,11 +1,19 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Map;
 
 
 @Controller
@@ -19,11 +27,17 @@ public class AdminController {
     }
 
     @GetMapping()
-    public String getAllUsers(Model model) {
+    public String getAllUsers(Model model, Principal principal) {
+
+//        String email = principal.getName();
+//
+//        model.addAttribute("email", email);
+
+        Long id = userService.getUserByUsername(principal.getName()).getId();
+        model.addAttribute("user", userService.getUserById(id));
         model.addAttribute("users", userService.getAllUsers());
         return "index";
     }
-
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Long id, Model model) {
